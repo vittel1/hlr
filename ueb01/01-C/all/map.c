14ea7c4a6b5e83a272a051e0df2a9609a7e01048
vittel1 <marcelpape@msn.com>
2019-10-18 18:17:33 +0200

    fix makefile

--- ueb01/01-C/all/map.c
+++ ueb01/01-C/all/map.c
@@ -1,82 +1,54 @@
 #include <stdio.h>
 
 // Definieren Sie ein enum cardd
-enum cardd{
+typedef enum {
 	N=1, E=2, S=4, W=8
-};
+} cardd;
 
 // Definieren Sie ein 3x3-Array namens map, das Werte vom Typ cardd enthält
-enum cardd map[3][3];
+cardd map[3][3];
 
 // Die Funktion set_dir soll an Position x, y den Wert dir in das Array map eintragen
 // Überprüfen Sie x und y um mögliche Arrayüberläufe zu verhindern
 // Überprüfen Sie außerdem dir auf Gültigkeit
-void set_dir (int x, int y, enum cardd dir)
-{
-	if (x < 0 || x > 2 || y < 0 || x > 2 || !dir){
-		return;
+void set_dir (int x, int y, cardd dir) {
+	if((x >= 0 && x <= 2)&&(y >= 0 && y <= 2)) {
+		map[x][y] = dir;
 	}
-	map[x][y] = dir;
 }
 
 // Die Funktion show_map soll das Array in Form einer 3x3-Matrix ausgeben
-void show_map (void)
-{
-	for (int row = 0; row < 3; row++){
-		for (int column = 0; column < 3; column++){
-			enum cardd value = map[row][column];
-			int combined = 0;
-			
-			switch (value)
-			{
-			case N|W:
-				printf("NW");
-				combined = 1;
-				break;
-			case S|W:
-				printf("SW");
-				combined = 1;
-				break;
-			case S|E:
-				printf("NE");
-				combined = 1;
-				break;
-			case N|E:
-				printf("SE");
-				combined = 1;
-				break;
-			case N:
-				printf("N");
-				break;
-			case S:
-				printf("S");
-				break;
-			case W:
-				printf("W");
-				break;
-			
-			case E:
-				printf("E");
-				break;
-			
-			default:
-				printf("0");
-				break;
-			}
-			if (column < 2){
-				if (combined){
-					printf("  ");
-				}else {
-					printf("   ");
-				}
+// Bitwise or: 0001|1000 = 1001 -> 9
+void show_map (void) {
+	
+	for (int x = 0; x < 3; x++) {
+		for (int y = 0; y < 3; y++) {
+			switch(map[x][y]) {
+				case 1: printf("N  "); break;
+				case 2: printf("E  "); break;
+				case 4: printf("S  "); break;
+				case 8: printf("W  "); break;
+				
+				//Pos (0/2). N|S. Case 3 ist unnoetig da ueberschrieben von N|S
+				case 5: printf("NE  "); break;
+
+				//Pos: (0/0): N|W: 0001|1000 = 1001 = 9
+				case 9: printf("NW  "); break;
+
+				//Pos: (2/2). E|W. 0010|1000 = 1010 = 10. Wird auch überschrieben.
+				case 10: printf("SE  "); break;
+	
+				//Pos (0/2). N|E.
+				case 12: printf("SW  "); break;
+
+				default: printf("0  "); break;
 			}
 		}
 		printf("\n");
 	}
 }
 
-int main (void)
-{
+int main (void) {
 	// In dieser Funktion darf nichts verändert werden!
 	set_dir(0, 1, N);
 	set_dir(1, 0, W);
