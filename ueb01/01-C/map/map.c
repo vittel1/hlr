26e256f120949eb18404d56450d9bc82437dd8a5
vittel1 <marcelpape@msn.com>
2019-10-18 19:17:54 +0200

    check fuer himmelsrichtung

--- ueb01/01-C/map/map.c
+++ ueb01/01-C/map/map.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 // Definieren Sie ein enum cardd
 typedef enum {
@@ -8,11 +9,30 @@ typedef enum {
 // Definieren Sie ein 3x3-Array namens map, das Werte vom Typ cardd enthält
 cardd map[3][3];
 
+//Überprüft, ob die übergebene Himmmelsrichtung gültig ist.
+bool is_proper_dir(cardd dir)
+{
+	switch(dir)
+	{
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 6:
+		case 8:
+		case 9:
+		case 12: return true;
+
+		default: return false;
+	}
+}
+
+
 // Die Funktion set_dir soll an Position x, y den Wert dir in das Array map eintragen
 // Überprüfen Sie x und y um mögliche Arrayüberläufe zu verhindern
 // Überprüfen Sie außerdem dir auf Gültigkeit
 void set_dir (int x, int y, cardd dir) {
-	if((x >= 0 && x <= 2)&&(y >= 0 && y <= 2)) {
+	if((x >= 0 && x <= 2)&&(y >= 0 && y <= 2) && is_proper_dir(dir)) {
 		map[x][y] = dir;
 	}
 }
@@ -29,14 +49,14 @@ void show_map (void) {
 				case 4: printf("S  "); break;
 				case 8: printf("W  "); break;
 				
-				//Pos (0/2). N|S. Case 3 ist unnoetig da ueberschrieben von N|S
-				case 5: printf("NE  "); break;
+				//Pos (0/2). N|E. 0001|0010 = 0011 = 3
+				case 3 : printf("NE  "); break;
+
+				//Pos: (2/2). S|E. 0010|1000 = 1010 = 10
+				case 6: printf("SE  "); break;
 
 				//Pos: (0/0): N|W: 0001|1000 = 1001 = 9
 				case 9: printf("NW  "); break;
-
-				//Pos: (2/2). E|W. 0010|1000 = 1010 = 10. Wird auch überschrieben.
-				case 10: printf("SE  "); break;
 	
 				//Pos (0/2). N|E.
 				case 12: printf("SW  "); break;
