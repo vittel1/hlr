a1a49e6cf55e89aedf29631728d7d5df4e63dd39
7schroet <niclas.schroeter@googlemail.com>
2019-10-31 15:35:58 +0100

    Speicheroptimierung

--- ueb03/03-PDE/pde/partdiff-seq.c
+++ ueb03/03-PDE/pde/partdiff-seq.c
@@ -224,10 +224,25 @@ calculate (struct calculation_arguments* arguments, struct calculation_results *
 		maxresiduum = 0;
 
 		/* over all rows */
-		for (j = 1; j < N; j++)
-		{
+		//for (j = 1; j < N; j++)
+		//{
 			/* over all columns */
-			for (i = 1; i < N; i++)
+		/*	for (i = 1; i < N; i++)
+			{
+				star = -Matrix[m2][i-1][j] - Matrix[m2][i][j-1] - Matrix[m2][i][j+1] - Matrix[m2][i+1][j] + 4.0 * Matrix[m2][i][j];
+
+				residuum = getResiduum(arguments, options, i, j, star);
+				korrektur = residuum;
+				residuum = (residuum < 0) ? -residuum : residuum;
+				maxresiduum = (residuum < maxresiduum) ? maxresiduum : residuum;
+
+				Matrix[m1][i][j] = Matrix[m2][i][j] + korrektur;
+			}
+		}*/
+
+		for(i = 1; i < N; i++)
+		{
+			for(j = 1; j < N; j++)
 			{
 				star = -Matrix[m2][i-1][j] - Matrix[m2][i][j-1] - Matrix[m2][i][j+1] - Matrix[m2][i+1][j] + 4.0 * Matrix[m2][i][j];
 
